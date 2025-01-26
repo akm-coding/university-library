b5fe67d85376c1c113da0fc46c2b21031de70907
Aung Kaung Myat <115795473+Aung-kaung-myat-coding@users.noreply.github.com>
2025-01-26 13:58:23 +0700

    feat: integrate imagekit for image upload, and develop FileUpload component, integrate public, and private key in .env

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 
 import localFont from "next/font/local";
 import "./globals.css";
+import { Toaster } from "@/components/ui/toaster";
 
 const ibmPlexSans = localFont({
   src: [
@@ -36,6 +37,8 @@ export default function RootLayout({
         className={`${ibmPlexSans.className} ${bebasNeue.variable} antialiased`}
       >
         {children}
+
+        <Toaster />
       </body>
     </html>
   );
